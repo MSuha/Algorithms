7137df432d83ebafd4be11a399f0a77a682ef334
muhammedsuhademirel <msuhademirel@gmail.com>
2019-09-18 16:43:01 +0300

    Fibonacci

---
+++ Dynamic Programming/Fibonacci.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int fibonacci(int n){
+
+    int Fib[n+1];
+
+    Fib[0] = 0;
+    Fib[1] = 1;
+
+    for(int i = 2; i < n; i++){
+        Fib[i] = Fib[i-1] + Fib[i-2];
+    }
+
+    return Fib[n];
+}
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    cout << fibonacci(n);
+    return 0;
+}
